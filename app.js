449d903e1223e97f8b595afb4a0eaad29fa3b780
toysow1 <143087015+toysow1@users.noreply.github.com>
2023-08-24 04:16:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_y6s8tat", "template_ximbuaj", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
